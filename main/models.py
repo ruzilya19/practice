b3873c40c8e0ff85eea3ef3a244e0ef9dc2ee8e1
practice <[ruzikva2019@mail.ru]>
2024-03-11 21:26:05 +0300

    migrate

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=12, decimal_places=2) 
+    discount_price = models.DecimalField(
+        max_digits=12, decimal_places=2, null=True
+    )
+    count = models.IntegerField(default=0)
